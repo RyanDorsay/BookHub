69baedb1216d3648d7b124c10f9cdc19103ffd30
Ryan Dorsay <ryan.dorsay@yahoo.com>
2025-03-06 14:15:51 -0500

    updated index page

--- obj/Debug/net9.0/BookHub.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d6f9c78dabc4fee2531fd03554e6cf71eb49ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d679f8cdb7944880622975ddedc4d4be7fceef")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
